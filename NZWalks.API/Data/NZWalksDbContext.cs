a10f2272acbb6fba04915f9b59a1f1922d972d11
Cristian Hernandez <chinorubikguitar@hotmail.com>
2023-06-23 21:11:07 -0600

    Seeding some data to the table of Regions and difficulties

--- NZWalks.API/Data/NZWalksDbContext.cs
+++ NZWalks.API/Data/NZWalksDbContext.cs
@@ -13,5 +13,64 @@ namespace NZWalks.API.Data
     public DbSet<Region> Regions { get; set; }
     public DbSet<Walk> Walks { get; set; }
 
-  }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Seed data for difficulties
+            // Easy, Medium, Hard
+
+            var difficulties = new List<Difficulty>()
+            {
+                new Difficulty()
+                {
+                    Id = Guid.Parse("3897b275-7a3f-4a84-a620-105b9b0eb89a"),
+                    name = "Easy",
+                },
+                new Difficulty()
+                {
+                    Id = Guid.Parse("e4567686-1b4d-483d-a374-9e99306c8e7b"),
+                    name = "Medium",
+                },
+                new Difficulty()
+                {
+                    Id = Guid.Parse("de63304d-8500-4570-8333-abb077e5a23f"),
+                    name = "Hard",
+                },
+            };
+
+            // seed difficulties to the database
+            modelBuilder.Entity<Difficulty>().HasData(difficulties);
+
+
+            var regions = new List<Region>()
+            {
+                new Region()
+                {
+                    Id = Guid.Parse("8929b4bf-5be3-4002-8ad6-b9f46f782f16"),
+                    Name = "Ciudad de México",
+                    Code = "CDMX",
+                    RegionImageUrl = "https://th.bing.com/th/id/OIP.YlInlV-4E257n6usFJv1GgHaFJ?pid=ImgDet&rs=1"
+                },
+                new Region()
+                {
+                    Id = Guid.Parse("67a04df2-e37a-4e80-8f3b-d52ba62f65e8"),
+                    Name = "Guadalajara",
+                    Code = "GDL",
+                    RegionImageUrl = "https://th.bing.com/th/id/R.3ee144293cd7e27e7d24d62d6ac6da95?rik=ojfVhRArgDl8PA&pid=ImgRaw&r=0"
+                },
+
+            };
+
+            // seed Regions to the database
+            modelBuilder.Entity<Region>().HasData(regions);
+
+            //MAKE MIGRATIONS TO SEE DATA IN DB
+
+            // Add-Migration "comment"
+            // Update-Database
+        }
+
+    }
 }
---
+++ NZWalks.API/Migrations/20230624030731_Seeding data for difficulties and regions.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace NZWalks.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class Seedingdatafordifficultiesandregions : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Regions",
+                columns: new[] { "Id", "Code", "Name", "RegionImageUrl" },
+                values: new object[,]
+                {
+                    { new Guid("67a04df2-e37a-4e80-8f3b-d52ba62f65e8"), "GDL", "Guadalajara", "https://th.bing.com/th/id/R.3ee144293cd7e27e7d24d62d6ac6da95?rik=ojfVhRArgDl8PA&pid=ImgRaw&r=0" },
+                    { new Guid("8929b4bf-5be3-4002-8ad6-b9f46f782f16"), "CDMX", "Ciudad de México", "https://th.bing.com/th/id/OIP.YlInlV-4E257n6usFJv1GgHaFJ?pid=ImgDet&rs=1" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "difficulties",
+                columns: new[] { "Id", "name" },
+                values: new object[,]
+                {
+                    { new Guid("3897b275-7a3f-4a84-a620-105b9b0eb89a"), "Easy" },
+                    { new Guid("de63304d-8500-4570-8333-abb077e5a23f"), "Hard" },
+                    { new Guid("e4567686-1b4d-483d-a374-9e99306c8e7b"), "Medium" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Regions",
+                keyColumn: "Id",
+                keyValue: new Guid("67a04df2-e37a-4e80-8f3b-d52ba62f65e8"));
+
+            migrationBuilder.DeleteData(
+                table: "Regions",
+                keyColumn: "Id",
+                keyValue: new Guid("8929b4bf-5be3-4002-8ad6-b9f46f782f16"));
+
+            migrationBuilder.DeleteData(
+                table: "difficulties",
+                keyColumn: "Id",
+                keyValue: new Guid("3897b275-7a3f-4a84-a620-105b9b0eb89a"));
+
+            migrationBuilder.DeleteData(
+                table: "difficulties",
+                keyColumn: "Id",
+                keyValue: new Guid("de63304d-8500-4570-8333-abb077e5a23f"));
+
+            migrationBuilder.DeleteData(
+                table: "difficulties",
+                keyColumn: "Id",
+                keyValue: new Guid("e4567686-1b4d-483d-a374-9e99306c8e7b"));
+        }
+    }
+}
